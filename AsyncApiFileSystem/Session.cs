251ff1c7708ebc65f2ef6f5c7f40f07b4edf47f0
Ugur-Arikan <uurfun@gmail.com>
2023-02-23 22:12:30 +0100

    Session static class is defined

--- AsyncApiFileSystem/Session.cs
+++ AsyncApiFileSystem/Session.cs
@@ -2,11 +2,30 @@
 
 namespace AsyncApiFileSystem;
 
-public class Session<Job, In> : SessionWrapper<Job, In, IdFactString, string>
-    where Job : IJob<string, In>
+/// <summary>
+/// Static class for creating session variants.
+/// </summary>
+public static class Session
 {
-	public Session()
-	{
-
-	}
+    /// <summary>
+    /// Constructs and returns the session if succeeds; the error if the constructor fails.
+    /// 
+    /// <inheritdoc cref="SessionCore{Job, In, IdFact, Id}"/>
+    /// 
+    /// <para>
+    /// Default session generic over input <typeparamref name="In"/> and job <typeparamref name="Job"/>, while having:
+    /// </para>
+    /// 
+    /// <list type="bullet">
+    /// <item>Id = string</item>
+    /// <item>IdFact = <see cref="IdFactString"/></item>
+    /// </list>
+    /// </summary>
+    /// <typeparam name="Job">Type of the job.</typeparam>
+    /// <typeparam name="In">Type of inputs of jobs.</typeparam>
+    /// <param name="rootDirectory">Root directory of jobs of the session.</param>
+    /// <param name="jobResults">Names of results of jobs.</param>
+    public static Res<SessionDefault<Job, In>> Default<Job, In>(string rootDirectory, Opt<HashSet<string>> jobResults = default)
+        where Job : IJob<string, In>
+        => SessionDefault<Job, In>.New(rootDirectory, jobResults);
 }
---
+++ AsyncApiFileSystem/SessionDefault.cs
@@ -0,0 +1,29 @@
+﻿using AsyncApiFileSystem.Commons;
+
+namespace AsyncApiFileSystem;
+
+/// <summary>
+/// <inheritdoc cref="SessionCore{Job, In, IdFact, Id}"/>
+/// 
+/// <para>
+/// Default session generic over input <typeparamref name="In"/> and job <typeparamref name="Job"/>, while having:
+/// </para>
+/// 
+/// <list type="bullet">
+/// <item>Id = string</item>
+/// <item>IdFact = <see cref="IdFactString"/></item>
+/// </list>
+/// </summary>
+/// <typeparam name="Job">Type of the job.</typeparam>
+/// <typeparam name="In">Type of inputs of jobs.</typeparam>
+public class SessionDefault<Job, In> : SessionWrapper<Job, In, IdFactString, string>
+    where Job : IJob<string, In>
+{
+    SessionDefault(ISession<Job, In, IdFactString, string> session) : base(session) { }
+    internal static Res<SessionDefault<Job, In>> New(string rootDirectory, Opt<HashSet<string>> jobResults)
+    {
+        return
+            SessionCore<Job, In, IdFactString, string>.New(rootDirectory, new(), jobResults.UnwrapOr(new HashSet<string>()))
+            .Map(session => new SessionDefault<Job, In>(session));
+    }
+}
